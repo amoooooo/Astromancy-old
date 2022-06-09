c4bbdca979a92f6ffc8a67bcda57476f683f6cf3
Jared <jaredlll08@gmail.com>
2022-06-09 17:19:55 +0200

    Fix the ArmSphere having incorrect matches

--- src/main/java/coffee/amo/astromancy/common/blockentity/ArmillarySphereBlockEntity.java
+++ src/main/java/coffee/amo/astromancy/common/blockentity/ArmillarySphereBlockEntity.java
@@ -3,12 +3,9 @@ package coffee.amo.astromancy.common.blockentity;
 import coffee.amo.astromancy.aequivaleo.AspectiEntry;
 import coffee.amo.astromancy.aequivaleo.AspectiHelper;
 import coffee.amo.astromancy.aequivaleo.AspectiInstance;
-import coffee.amo.astromancy.aequivaleo.compound.AspectiCompoundType;
-import coffee.amo.astromancy.core.registration.AspectiRegistry;
 import coffee.amo.astromancy.core.systems.aspecti.Aspecti;
-import coffee.amo.astromancy.core.systems.aspecti.AspectiMap;
+import com.google.common.collect.Lists;
 import com.sammy.ortus.helpers.BlockHelper;
-import com.sammy.ortus.helpers.util.Pair;
 import com.sammy.ortus.systems.blockentity.ItemHolderBlockEntity;
 import com.sammy.ortus.systems.blockentity.OrtusBlockEntityInventory;
 import net.minecraft.core.BlockPos;
@@ -23,16 +20,15 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 // TODO: fix inventory
 public class ArmillarySphereBlockEntity extends ItemHolderBlockEntity {
     public boolean toggled = false;
     public int ticksActive = 0;
     public boolean requirementBool = false;
-    public List<Pair<Aspecti, Integer>> requirements = new ArrayList<>();
-    public List<Pair<Aspecti, Integer>> contains = new ArrayList<>();
+    public Map<Aspecti, Integer> requirements = new HashMap<>();
+    public Map<Aspecti, Integer> contains = new HashMap<>();
 
     public ArmillarySphereBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
@@ -48,66 +44,62 @@ public class ArmillarySphereBlockEntity extends ItemHolderBlockEntity {
     @Override
     public void tick() {
         super.tick();
+        if (!level.isClientSide && !requirementBool){
+            generateRequirements(level);
+        }
         if (!level.isClientSide && toggled) {
             ticksActive++;
             if (ticksActive >= 60) {
                 toggled = false;
                 ticksActive = 0;
             }
-        } else if (!level.isClientSide && !requirementBool){
-            generateRequirements(level);
         }
     }
 
     public void generateRequirements(Level level){
-        int aspectiCount = level.random.nextInt(5);
-        for(int i = 0; i < aspectiCount; i++){
-            Aspecti aspecti = AspectiMap.intMap.get(level.random.nextInt(AspectiMap.intMap.size()));
+        int aspectiCount = level.random.nextInt(5) + 1;
+        List<Aspecti> shuffledAspecti = Lists.newArrayList(Aspecti.values());
+        Collections.shuffle(shuffledAspecti);
+        shuffledAspecti.subList(0, aspectiCount).forEach(aspecti -> {
             int amount = level.random.nextInt(31);
-            requirements.add(Pair.of(aspecti, amount));
-        }
+            requirements.put(aspecti, amount);
+        });
         requirementBool = true;
     }
 
-    public boolean checkInventoryForRequirements(){
-        List<Pair<Aspecti, Integer>> totalRequirements = new ArrayList<>();
-        for(int i = 0; i < inventory.getSlots(); i++){
-            ItemStack item = inventory.getStackInSlot(i);
-            List<AspectiInstance> instance = AspectiHelper.getEntry(level.dimension(), item).aspecti;
-            for(AspectiInstance inst : instance){
-                for(Pair<Aspecti, Integer> current : totalRequirements){
-                    if(current.getFirst().equals(inst.type.aspecti)){
-                        current.setSecond((int) (current.getSecond() + inst.amount));
-                    }
-                    else {
-                        totalRequirements.add(Pair.of(inst.type.aspecti, (int)Math.ceil(inst.amount)));
-                    }
-                }
+    public Map<Aspecti, Integer> getMatchFromInventory(){
+        Map<Aspecti, Integer> match = new HashMap<>();
+        for(ItemStack itemStack : inventory.getStacks()){
+            if(itemStack.isEmpty()){
+                continue;
+            }
+            AspectiEntry entry = AspectiHelper.getEntry(level.dimension(), itemStack);
+
+            for (AspectiInstance instance : entry.aspecti) {
+                int count = (int)Math.ceil(instance.amount);
+                match.compute(instance.type.aspecti, (aspecti, integer) -> integer == null ? count : integer + count);
             }
         }
-        boolean match = false;
-        for(Pair<Aspecti, Integer> req : requirements){
-            for(Pair<Aspecti, Integer> total : totalRequirements){
-                if(total.getFirst().equals(req.getFirst())){
-                    if(total.getSecond() >= req.getSecond()){
-                        match = true;
-                    } else if (total.getSecond() < req.getSecond()){
-                        match = false;
-                    }
-                }
-            }
-        }
-        System.out.println(totalRequirements);
         return match;
+    }
+
+    public boolean checkMatch(Map<Aspecti, Integer> match) {
+
+        return requirements.entrySet().stream().allMatch(entry -> {
+            Integer actual = match.get(entry.getKey());
+            return actual != null && actual >= entry.getValue();
+        });
     }
 
     @Override
     public InteractionResult onUse(Player player, InteractionHand hand) {
         if (player.getItemInHand(hand).isEmpty() && !player.isShiftKeyDown() && inventory.getSlots() == 12) {
-            toggled = !toggled;
-            System.out.println(requirements);
-            if(checkInventoryForRequirements()){
-                player.sendMessage(new TextComponent("Match!"), player.getUUID());
+            if(!player.level.isClientSide) {
+                toggled = !toggled;
+                System.out.println(requirements);
+                if (checkMatch(getMatchFromInventory())) {
+                    player.sendMessage(new TextComponent("Match!"), player.getUUID());
+                }
             }
             return InteractionResult.SUCCESS;
         } else if (player.getItemInHand(hand).isEmpty() && player.isShiftKeyDown()) {
