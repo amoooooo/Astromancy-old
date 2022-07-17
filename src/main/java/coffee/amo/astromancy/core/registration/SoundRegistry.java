f7b7e0ee1ab0c36f56ed6a3cfe58dba6a89fc7d8
amo <lunarteclipse@gmail.com>
2022-07-17 02:01:08 +0100

    jar sounds

--- src/main/java/coffee/amo/astromancy/core/registration/SoundRegistry.java
+++ src/main/java/coffee/amo/astromancy/core/registration/SoundRegistry.java
@@ -10,7 +10,11 @@ public class SoundRegistry {
     public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, Astromancy.MODID);
 
     public static final RegistryObject<SoundEvent> RESEARCH_WRITE = register(new SoundEvent(Astromancy.astromancy("write")));
-    public static final RegistryObject<SoundEvent> JAR = register(new SoundEvent(Astromancy.astromancy("jar")));
+    public static final RegistryObject<SoundEvent> JAR_BREAK = register(new SoundEvent(Astromancy.astromancy("jar_break")));
+    public static final RegistryObject<SoundEvent> JAR_PLACE = register(new SoundEvent(Astromancy.astromancy("jar_place")));
+    public static final RegistryObject<SoundEvent> JAR_HIT = register(new SoundEvent(Astromancy.astromancy("jar_hit")));
+    public static final RegistryObject<SoundEvent> EMPTY_JAR = register(new SoundEvent(Astromancy.astromancy("empty_jar")));
+    public static final RegistryObject<SoundEvent> FILL_JAR = register(new SoundEvent(Astromancy.astromancy("fill_jar")));
     public static final RegistryObject<SoundEvent> GRIND = register(new SoundEvent(Astromancy.astromancy("grind")));
 
     public static RegistryObject<SoundEvent> register(SoundEvent soundEvent) {
--- src/main/java/coffee/amo/astromancy/core/registration/SoundTypes.java
+++ src/main/java/coffee/amo/astromancy/core/registration/SoundTypes.java
@@ -4,5 +4,6 @@ import net.minecraft.world.level.block.SoundType;
 import net.minecraftforge.common.util.ForgeSoundType;
 
 public class SoundTypes {
-    public static final SoundType JAR = new ForgeSoundType(1, 1, SoundRegistry.JAR, SoundRegistry.JAR, SoundRegistry.JAR, SoundRegistry.JAR, SoundRegistry.JAR);
+    //break, step, place, hit, fall
+    public static final SoundType JAR = new ForgeSoundType(1, 1, SoundRegistry.JAR_BREAK, SoundRegistry.JAR_HIT, SoundRegistry.JAR_PLACE, SoundRegistry.JAR_HIT, SoundRegistry.JAR_HIT);
 }
